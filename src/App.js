b3953f76319e21a46c9c6cce7726ab8a70ebf210
Teo <WorkCoding@Teodors-MacBook-Pro.local>
2022-01-23 17:23:12 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Button from "./ChildishComponent/Button.jsx";
+import "./index.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  let incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  let decrementCount = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="app">
+      <div>
+        <div class="count">
+          <h3>Count:</h3>
+          <h1>{count}</h1>
+        </div>
+        <div class="buttons">
+          <Button title={"-"} action={decrementCount} />
+          <Button title={"+"} action={incrementCount} />
+        </div>
+      </div>
+    </div>
+  );
+}
